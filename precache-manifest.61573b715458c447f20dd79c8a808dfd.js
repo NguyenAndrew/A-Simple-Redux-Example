4d3599b318a012cc861e0256c44630046478c24e
NguyenAndrew <anguyen29@wisc.edu>
2019-03-10 01:56:21 -0500

    Updates

---
+++ precache-manifest.61573b715458c447f20dd79c8a808dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66ef6080bd401cf83bee",
+    "url": "/A-Simple-Redux-Example/static/js/runtime~main.66ef6080.js"
+  },
+  {
+    "revision": "ad2fe15859c3d4503fb6",
+    "url": "/A-Simple-Redux-Example/static/js/main.ad2fe158.chunk.js"
+  },
+  {
+    "revision": "2c2234c30ed52f221f19",
+    "url": "/A-Simple-Redux-Example/static/js/2.2c2234c3.chunk.js"
+  },
+  {
+    "revision": "ad2fe15859c3d4503fb6",
+    "url": "/A-Simple-Redux-Example/static/css/main.cb9473ef.chunk.css"
+  },
+  {
+    "revision": "5d339dc29499ae75a422d0bf7d2e7b0f",
+    "url": "/A-Simple-Redux-Example/index.html"
+  }
+];
